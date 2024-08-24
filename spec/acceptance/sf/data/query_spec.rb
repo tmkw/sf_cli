38b516168be435543f78f1b5c3d9b42b68894d79
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-08-24 09:49:20 +0900

    0.0.1/refactoring: sf data (acceptance test)

--- spec/acceptance/sf/data/query_spec.rb
+++ spec/acceptance/sf/data/query_spec.rb
@@ -3,14 +3,25 @@ require 'spec_helper'
 RSpec.describe 'sf data query' do
   let(:sf) { SfCli::Sf.new }
 
-  it "queries with SOQL" do
+  before do
     allow(sf).to receive(:`).with('sf data query --query "SELECT Id, Name FROM Account LIMIT 1" --json 2> /dev/null').and_return(command_response)
+  end
 
+  it "queries with SOQL" do
     rows = sf.data.query %|SELECT Id, Name FROM Account LIMIT 1|
 
     expect(rows).to contain_exactly({'Id' => "0015j00001dsDuhAAE", 'Name' => "Aethna Home Products"})
 
     expect(sf).to have_received(:`)
+  end
+
+  it "can convert each record into a paticular model object" do
+    Account = Struct.new(:Id, :Name)
+
+    rows = sf.data.query %|SELECT Id, Name FROM Account LIMIT 1|, model_class: Account
+
+    expect(rows).to contain_exactly( an_object_having_attributes('Id' => "0015j00001dsDuhAAE", 'Name' => "Aethna Home Products"))
+    expect(rows.first).to be_instance_of Account
   end
 
   def command_response
