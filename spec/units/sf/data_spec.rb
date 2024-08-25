af16f0d11bf9bf7e634a5ff6968fc01179cf49ee
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-08-25 14:46:49 +0900

    0.0.3/feat-0002: sf data create record

--- spec/units/sf/data_spec.rb
+++ spec/units/sf/data_spec.rb
@@ -146,6 +146,53 @@ RSpec.describe 'SfCli::Sf::Data' do
     end
   end
 
+  describe '#create_record' do
+    let(:object_type) { :TestCustomObject__c  }
+    let(:new_record_id) { 'abcdefg'  }
+
+    it "creates a record" do
+      allow(sf).to receive(:exec).with(
+        'data',
+        'create record',
+        flags: {
+          sobject:      object_type,
+          values:        %|"Name='bar hoge' Age=52"|,
+          :"target-org" => nil,
+        },
+        switches: {},
+        redirection: :null_stderr
+      )
+      .and_return(create_response)
+
+      id = data.create_record object_type, values: {Name: 'bar hoge', Age: 52}
+
+      expect(id).to eq new_record_id
+      expect(sf).to have_received :exec
+    end
+
+    context 'using option: target_org' do
+      it "creates an record in the paticular org, not default one" do
+        allow(sf).to receive(:exec).with(
+          'data',
+          'create record',
+          flags: {
+            sobject:      object_type,
+            values:        %|"Name='bar hoge' Age=52"|,
+            :"target-org" => :dev,
+          },
+          switches: {},
+          redirection: :null_stderr
+        )
+        .and_return(create_response)
+
+        id = data.create_record object_type, values: {Name: 'bar hoge', Age: 52}, target_org: :dev
+
+        expect(id).to eq new_record_id
+        expect(sf).to have_received :exec
+      end
+    end
+  end
+
   def query_response
     {
       "status" => 0,
@@ -177,6 +224,18 @@ RSpec.describe 'SfCli::Sf::Data' do
         },
         "Id" => "#{record_id}",
         "Name" => "Akin Kristen"
+      },
+      "warnings" => []
+    }
+  end
+
+  def create_response
+    {
+      "status" => 0,
+      "result" => {
+        "id" => "#{new_record_id}",
+        "success" => true,
+        "errors" => []
       },
       "warnings" => []
     }
