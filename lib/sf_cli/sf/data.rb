e402dc5066e62476f7d4708a536e36788c55b582
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-08-25 16:22:12 +0900

    0.0.3/feat-0003: sf data update record

--- lib/sf_cli/sf/data.rb
+++ lib/sf_cli/sf/data.rb
@@ -66,6 +66,34 @@ module SfCli
 
         model_class ? model_class.new(**result) : result
       end
+
+      # update a object record. (eqivalent to *sf* *data* *update* *record*)
+      #
+      # *object_type* --- Object Type (ex. Account)<br>
+      # *record_id* --- id of the object<br>
+      # *where* --- field values that is used to identify a record<br>
+      # *values* --- field values for update<br>
+      # *target_org* --- an alias of paticular org, not default one<br>
+      #
+      # ==== examples
+      #   sf.data.update_record :Account, record_id: 'xxxxxxx', values: {Name: 'New Account Name'}
+      #   sf.data.update_record :Hoge__c, where: {Name: 'Jonny B.Good', Country: 'USA'}, values: {Phone: 'xxxxx', bar: 2000}
+      #
+      def update_record(object_type, record_id: nil, where: nil, values: nil, target_org: nil)
+        where_conditions  = field_value_pairs(where)
+        field_values      = field_value_pairs(values)
+        flags = {
+          :"sobject"    => object_type,
+          :"record-id"  => record_id,
+          :"where"      => (where_conditions.nil? ? nil : %|"#{where_conditions}"|),
+          :"values"     => (field_values.nil? ? nil : %|"#{field_values}"|),
+          :"target-org" => target_org,
+        }
+        action = __method__.to_s.tr('_', ' ')
+        json = exec(action, flags: flags, redirection: :null_stderr)
+
+        json['result']['id']
+      end
     end
   end
 end
