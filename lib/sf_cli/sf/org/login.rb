9f4e1aa08d9e178fe3893a6bd57efd05660808a5
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-09-08 05:54:56 +0900

    0.0.5/feat support sf org list

---
+++ lib/sf_cli/sf/org/login.rb
@@ -0,0 +1,23 @@
+module SfCli::Sf::Org
+  module Login
+    # login to the org by the browser. (equivalent to *sf* *org* *login* *web*)
+    #
+    # *target_org* --- an alias of paticular org, not default one<br>
+    # *instance_url* --- custom login url.
+    #
+    # == examples:
+    #   sf.org.login_web
+    #   sf.org.login_web target_org: :dev  # if the org you login isn't the default one, you should give it alias name for later use.
+    #
+    # For more command details, see {the command reference}[https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_org_commands_unified.htm#cli_reference_org_login_web_unified]
+    #
+    def login_web(target_org: nil, instance_url: nil)
+      flags    = {
+        :"alias"        => target_org,
+        :"instance-url" => instance_url,
+      }
+      action = __method__.to_s.tr('_', ' ')
+      exec(action, flags: flags)
+    end
+  end
+end
