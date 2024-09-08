8d8ac43aa8a43a8b9509efa8176f4d51f71ff813
tmkw <tmaekawa@jcom.home.ne.jp>
2024-09-08 11:22:03 +0900

    0.0.5/feat: support sf org login access-token (#52)

--- lib/sf_cli/sf/org/login.rb
+++ lib/sf_cli/sf/org/login.rb
@@ -12,13 +12,25 @@ module SfCli::Sf::Org
     # For more command details, see {the command reference}[https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_org_commands_unified.htm#cli_reference_org_login_web_unified]
     #
     def login_web(target_org: nil, instance_url: nil, browser: nil)
-      flags    = {
+      flags = {
         :"alias"        => target_org,
         :"instance-url" => instance_url,
         :"browser"      => browser,
       }
       action = __method__.to_s.tr('_', ' ')
       exec(action, flags: flags)
+    end
+
+    def login_access_token(instance_url:, target_org: nil)
+      flags = {
+        :"instance-url" => instance_url,
+        :"alias"        => target_org,
+      }
+      switches = {
+        :"no-prompt" => true,
+      }
+      action = __method__.to_s.tr('_', '-').sub('-', ' ')
+      exec action, flags: flags, switches: switches, redirection: :null_stderr
     end
   end
 end
