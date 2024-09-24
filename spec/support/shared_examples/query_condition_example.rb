cd4cf493dbd4be779fe2b2bcfd57e4f3606d93ca
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-09-24 14:53:43 +0900

    0.0.10/feat: aggregate function to object model

--- spec/support/shared_examples/query_condition_example.rb
+++ spec/support/shared_examples/query_condition_example.rb
@@ -52,3 +52,51 @@ RSpec.shared_examples 'QueryCondition#pluck' do
     expect(connection).to have_received :query
   end
 end
+
+RSpec.shared_examples 'QueryCondition#count' do
+  let(:query_condition) { SfCli::Sf::Model::QueryMethods::QueryCondition.new(connection, klass.name, field_names) }
+  let(:soql) { "SELECT COUNT(Id) FROM #{klass.name}" }
+  let(:rows) { [{'expr0' => 3}] }
+  let(:result) { 3 }
+
+  before do
+    allow(connection).to receive(:query).with(soql, nil).and_return(rows)
+  end
+
+  it 'returns record count that match the query condtions' do
+    expect(query_condition.count).to eq result
+    expect(connection).to have_received :query
+  end
+end
+
+RSpec.shared_examples 'QueryCondition#min' do
+  let(:query_condition) { SfCli::Sf::Model::QueryMethods::QueryCondition.new(connection, klass.name, field_names) }
+  let(:soql) { "SELECT MIN(LastModifiedDate) FROM #{klass.name}" }
+  let(:rows) { [{'expr0' => '2024-09-24T13:46:00.000+0000'}] }
+  let(:result) { '2024-09-24T13:46:00.000+0000' }
+
+  before do
+    allow(connection).to receive(:query).with(soql, nil).and_return(rows)
+  end
+
+  it 'returns record count that match the query condtions' do
+    expect(query_condition.min(:LastModifiedDate)).to eq result
+    expect(connection).to have_received :query
+  end
+end
+
+RSpec.shared_examples 'QueryCondition#max' do
+  let(:query_condition) { SfCli::Sf::Model::QueryMethods::QueryCondition.new(connection, klass.name, field_names) }
+  let(:soql) { "SELECT MAX(LastModifiedDate) FROM #{klass.name}" }
+  let(:rows) { [{'expr0' => '2024-09-24T13:46:00.000+0000'}] }
+  let(:result) { '2024-09-24T13:46:00.000+0000' }
+
+  before do
+    allow(connection).to receive(:query).with(soql, nil).and_return(rows)
+  end
+
+  it 'returns record count that match the query condtions' do
+    expect(query_condition.max(:LastModifiedDate)).to eq result
+    expect(connection).to have_received :query
+  end
+end
