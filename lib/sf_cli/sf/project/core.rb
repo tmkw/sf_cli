69ae750a3af35b7e123ebd92c30292370dc2ada0
tmkw <tmaekawa@jcom.home.ne.jp>
2024-09-27 01:01:46 +0900

    1.0.0: completes documentation (#140)

--- lib/sf_cli/sf/project/core.rb
+++ lib/sf_cli/sf/project/core.rb
@@ -9,6 +9,7 @@ module SfCli
     #
     # @see https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_project_commands_unified.htm command reference
     #
+    # @private :nodoc: just for developers
     module Project
       # @private :nodoc: just for developers
       class Core
