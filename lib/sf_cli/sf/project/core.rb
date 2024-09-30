18dfc5b604fcadc74c24ced972689056de91ffae
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-09-30 12:46:34 +0900

    1.2.0 get 'sf project' back as supported.

--- lib/sf_cli/sf/project/core.rb
+++ lib/sf_cli/sf/project/core.rb
@@ -9,7 +9,6 @@ module SfCli
     #
     # @see https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_project_commands_unified.htm command reference
     #
-    # @private :nodoc: just for developers
     module Project
       # @private :nodoc: just for developers
       class Core
