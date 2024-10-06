d25fd3ee3e4257e9cd1fc8f5c4cced3598f42978
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-10-07 04:43:44 +0900

    1.2.2/fix: model generator causes warnings when it is called multiple times

--- spec/support/shared_examples/generator_examples.rb
+++ spec/support/shared_examples/generator_examples.rb
@@ -1,20 +1,23 @@
 require 'sf_cli/sf/model/generator'
 
 RSpec.shared_examples 'Generator#generate' do
+  let(:generator) { SfCli::Sf::Model::Generator.new(connection) }
+
   before do
     allow(connection).to receive(:describe).with(object_name).and_return(schema)
     allow(class_definition).to receive(:to_s).and_return(class_expression)
   end
 
   it 'generates a class' do
-    generator = SfCli::Sf::Model::Generator.new(connection)
-
-    generator.generate(object_name)
-
+    expect(generator.generate(object_name)).to be true
     expect(Object.const_get object_name.to_sym).to be ModelGeneratorTestClass
     expect(ModelGeneratorTestClass.connection).to be connection
 
     expect(connection).to have_received :describe
     expect(class_definition).to have_received :to_s
+  end
+
+  it "doesn't generate class that aliready exists" do
+    expect(generator.generate(object_name)).to be false
   end
 end
