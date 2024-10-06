ad7af5749a0894b1bc1a8d63839f5ae76bc9bd76
tmkw <tmaekawa@jcom.home.ne.jp>
2024-10-07 04:47:37 +0900

    1.2.2/fix: model generator causes warnings when it is called multiple times (#194)

--- lib/sf_cli/sf/model/generator.rb
+++ lib/sf_cli/sf/model/generator.rb
@@ -12,16 +12,27 @@ module SfCli
         end
 
         def generate(object_name)
+          return false if generated? object_name
+
           schema = describe(object_name)
           class_definition = ClassDefinition.new(schema)
 
           instance_eval "::#{object_name} = #{class_definition}"
           klass = Object.const_get object_name.to_sym
           klass.connection = connection
+
+          true
         end
 
         def describe(object_name)
           connection.describe object_name
+        end
+
+        def generated?(object_name)
+          Object.const_get object_name.to_sym
+          true
+        rescue NameError
+          false
         end
       end
     end
