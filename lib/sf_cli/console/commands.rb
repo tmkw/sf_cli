0b3668b39e62f88468a5473867444428ec2d8929
tmkw <tmaekawa@jcom.home.ne.jp>
2024-11-20 18:33:46 +0900

    1.3.0/mantenance: documentation (#279)

--- lib/sf_cli/console/commands.rb
+++ lib/sf_cli/console/commands.rb
@@ -1,8 +1,5 @@
 require 'sf_cli'
 require 'byebug'
-require 'sf_cli/sf/model'
-require 'sf_cli/sf/model/query_condition'
-require 'sf_cli/sf/model/sf_command_connection'
 require 'stringio'
 
 module SfCli
@@ -12,36 +9,8 @@ module SfCli
     # Developer Console commands
     #
     module Commands
-      def use(target_org)
-        org_info = sf.org.display target_org: target_org
-        conn = SfCli::Sf::Model::SfCommandConnection.new target_org: target_org, instance_url: org_info.instance_url
-        conn.open unless org_info.connected?
-
-        SfCli::Sf::Model.set_connection conn
-
-        available_models.each do |model|
-          Object.const_get(model).connection = conn
-        end
-
-        true
-      end
-
-      def available_models
-        @available_models ||= []
-      end
-
-      def generate(*object_types)
-        SfCli::Sf::Model.generate object_types
-        available_models.append(*object_types).flatten
-        object_types
-      end
-
-      def connection
-        SfCli::Sf::Model.connection
-      end
-
       def target_org
-        connection.target_org
+        @target_org
       end
 
       def apex(apex_code = nil)
@@ -60,68 +29,6 @@ module SfCli
       def orgs
         conf.inspect_mode = false
         system 'sf org list'
-        conf.inspect_mode = true
-      end
-
-      alias :gen  :generate
-      alias :conn :connection
-
-      def help
-        conf.inspect_mode = false
-        puts <<~HELP
-          Available commands:
-            use   --- set current org.
-            gen   --- generate Object model classes
-            query --- Query by SOQL with human readable format
-            apex  --- run Apex code
-            conn  --- show current connection setting
-            orgs  --- show the list of org
-
-          Syntax:
-            [use]
-              use target-org
-
-              parameters:
-                targat-org --- Username or alias of the org you are going to use. If you are not sure about them, check by `sf org list`.
-
-              example:
-                use :your_org_alias
-
-            [gen]
-              gen object-name, object-name, ...
-              generate object-name, object-name, ...
-
-              parameters:
-                object-name --- Comma separated Names. Symbol or String can be OK. At least 1 object name is required.
-
-              example:
-                gen :Account, :Contact, :User
-
-            [query]
-              query SOQL
-
-                parameters:
-                  SOQL --- soql.You must quote it like "SELECT ...."
-
-              example:
-                query "SELECT Id, Name FROM Account LIMIT 3"
-
-            [apex]
-              apex apex_code
-
-              parameters:
-                apex code --- Apex code you want to execute.You must quote the code.
-
-              example:
-                apex "System.debug('abc');"
-
-            [conn]
-                conn
-                connection
-
-            [orgs]
-                orgs
-        HELP
         conf.inspect_mode = true
       end
     end
--- lib/sf_cli/sf/lightning/generate.rb
+++ lib/sf_cli/sf/lightning/generate.rb
@@ -1,5 +1,17 @@
 module SfCli::Sf::Lightning
   module Generate
+    # Generate an Apex class
+    # @param name        [String,Symbol] component name
+    # @param type        [String,Symbol] component type.Only aura or lwc is permissible.
+    # @param output_dir  [String]        directory for saving the created files
+    # @param template    [String,Symbol] Template to use for file creation.Permissible values are: default, analyticsDashboard, analyticsDashboardWithStep
+    # @param api_version [Numeric]       override the api version used for api requests made by this command
+    # @param raw_output  [Boolian]       output what original command shows
+    #
+    # @return [Array] the list of files that is created
+    #
+    # @see https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_lightning_commands_unified.htm#cli_reference_lightning_generate_component_unified command reference
+    #
     def generate_component(name, type: nil, output_dir: nil, template: nil, api_version: nil, raw_output: false)
       flags = {
         :"name" => name,
